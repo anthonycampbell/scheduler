b5ed35ce194448f3c03ceeedf93a19dcdac92ba1
Anthony Campbell <anthony-campbell@hotmail.com>
2022-09-06 20:09:23 -0700

    Implement Show Header and Confirm components for Appointments.

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,8 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 storiesOf("Button", module)
   .addParameters({
@@ -149,4 +151,10 @@ storiesOf("InterviewerList", module)
   ))
   .add("Empty", () => (
     <Empty onAdd={action("onAdd")} />
+  ))
+  .add("Show", () => (
+    <Show student="Lydia Miller-Jones" interviewer={interviewer.name} onEdit={action("onEdit")} onDelete={action("onDelete")} />
+  ))
+  .add("Confirm", () => (
+    <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />
   ))
