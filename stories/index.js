5798441587838f182d3730c2acd9078af31b8c2d
Anthony Campbell <anthony-campbell@hotmail.com>
2022-09-06 19:43:45 -0700

    Implement Empty appointment and Header components.

--- stories/index.js
+++ stories/index.js
@@ -11,7 +11,8 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
-import Header from "components/Appointment/Header"
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 storiesOf("Button", module)
   .addParameters({
@@ -142,4 +143,10 @@ storiesOf("InterviewerList", module)
   ))
   .add("Appointment with Time", () => (
     <Appointment time="12pm" />
+  ))
+  .add("Header", () => (
+    <Header time="12pm" />
+  ))
+  .add("Empty", () => (
+    <Empty onAdd={action("onAdd")} />
   ))
