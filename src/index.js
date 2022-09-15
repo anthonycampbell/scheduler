6d121a04e430e56c3eaf7e7e3608f003099a0b23
Anthony Campbell <anthony-campbell@hotmail.com>
2022-09-15 00:50:14 -0700

    Change env variables.

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import axios from "axios";
 
 if (process.env.REACT_APP_API_BASE_URL) {
   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+} else {
+  axios.defaults.baseURL = "http://localhost:8001";
 }
 
 ReactDOM.render(<Application />, document.getElementById("root"));
